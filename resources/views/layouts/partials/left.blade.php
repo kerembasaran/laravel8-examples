2a86a942816a18057994883e7a775d88c5ca563b
Kerem BAŞARAN <kerembasaran14@gmail.com>
2021-06-14 17:09:32 +0300

    Http client örnekleri

--- resources/views/layouts/partials/left.blade.php
+++ resources/views/layouts/partials/left.blade.php
@@ -1,5 +1,17 @@
 <div class="list-group">
     <a href="{{ route('home') }}" class="list-group-item list-group-item-action">Home</a>
     <a href="{{ route('route.index') }}" class="list-group-item list-group-item-action">Route</a>
+    <div class="dropdown dropright list-group-item list-group-item-action">
+        <div class="stretched-link" id="dropdownHttpClient" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Http Client >
+        </div>
+        <div class="dropdown-menu" aria-labelledby="dropdownHttpClient">
+            <a class="dropdown-item" href="{{ route('http-client.get-all-post') }}">Tüm Veriler</a>
+            <a class="dropdown-item" href="{{ route('http-client.get-post-by-id',7) }}">Tek Veriler</a>
+            <a class="dropdown-item" href="{{ route('http-client.add-post') }}">Veri Ekleme</a>
+            <a class="dropdown-item" href="{{ route('http-client.update-post') }}">Veri Güncelleme</a>
+            <a class="dropdown-item" href="{{ route('http-client.delete-post',12) }}">Veri Silme</a>
+        </div>
+    </div>
 </div>
 
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HttpClientController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,3 +19,8 @@ use Illuminate\Support\Facades\Route;
 });*/
 Route::view('/', 'home')->name('home');
 Route::view('route/index', 'route.index')->name('route.index');
+Route::get('http-client/get-all-post', [HttpClientController::class, 'getAllPost'])->name('http-client.get-all-post');
+Route::get('http-client/get-post-by-id/{id}', [HttpClientController::class, 'getPostById'])->name('http-client.get-post-by-id');
+Route::get('http-client/add-post', [HttpClientController::class, 'addPost'])->name('http-client.add-post');
+Route::get('http-client/update-post', [HttpClientController::class, 'updatePost'])->name('http-client.update-post');
+Route::get('http-client/delete-post/{id}', [HttpClientController::class, 'deletePost'])->name('http-client.delete-post');
