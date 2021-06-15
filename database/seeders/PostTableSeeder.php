f3b2afa6f482dacaf20ffd9ea2561e05b29e8ef6
Kerem BAŞARAN <kerembasaran14@gmail.com>
2021-06-15 15:28:44 +0300

    Seeder örneği

---
+++ database/seeders/PostTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+class PostTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        foreach (range(1, 100) as $index) {
+            DB::table('posts')->insert([
+                'title' => $faker->sentence(5),
+                'body' => $faker->paragraph(4)
+            ]);
+        }
+        /*
+        DB::table('sm_posts')->insert([
+            'title' => 'Seeder First Post Title',
+            'body' => 'Seeder First Post Description'
+        ]);
+        */
+    }
+}
